504cb98c5235cce0c5931262ea07013631bc4503
Kerstin Laptop <kerstin@irox-games.com>
2024-11-09 12:33:00 +0100

    include mock images + update plant model wrt state

--- femdevsjam24/Features/Plant/plant.gd
+++ femdevsjam24/Features/Plant/plant.gd
@@ -1,19 +1,27 @@
 extends Node2D
 
+@export_category("{PotSeed, Sapling, Little, Medium, Big, Blossom,	Withered}")
+@export var plant_state_images: Array[Texture2D]
+
 var clickable: Clickable
-
 var plant_state: Plants.States = Plants.States.PotSeed
-
+var plant_visual: Sprite2D
 
 func _ready() -> void:
+	plant_visual = $PlantVisual
 	clickable = $Clickable
 	clickable.connect("mouse_clicked_on_object", _on_clicked)
-
-
-func _process(delta: float) -> void:
-	pass
+	
+	update_visual()
 
 
 func _on_clicked() -> void:
 	print("plant was clicked")
 	# TODO:make task be executed
+	plant_state += 1
+	update_visual()
+
+func update_visual() -> void:
+	if plant_state >= plant_state_images.size():
+		return
+	plant_visual.texture = plant_state_images[plant_state]
