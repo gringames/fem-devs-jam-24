80c04a037b4d271ce04de89d8fb0d14893da9a89
Kerstin Laptop <kerstin@irox-games.com>
2024-11-09 02:02:33 +0100

    add dialogue box

--- femdevsjam24/Text/Box/text_box.gd
+++ femdevsjam24/Text/Box/text_box.gd
@@ -1,16 +1,21 @@
 extends Node2D
 class_name TextBox
 
+
+@export var visible_by_default: bool = false
+
 var text_label: RichTextLabel
+var npc_icon: TextureRect
 
 
 func _ready() -> void:
-	text_label = $TextureRect/RichTextLabel
+	text_label = $DialogueBox/BackgroundTexture/RichTextLabel
+	npc_icon = $DialogueBox/BackgroundTexture/NPCIcon
+	
+	if not visible_by_default:
+		_disappear()
 
-
-func _process(delta: float) -> void:
-	pass
-
+# TEXT ---------------------------------------------------------------------------------------------
 
 func set_text(text: String) -> void:
 	text_label.text = text;
@@ -20,6 +25,13 @@ func clear_text() -> void:
 	set_text("")
 
 
+# ICON ---------------------------------------------------------------------------------------------
+
+func set_icon(icon: Texture2D) -> void:
+	npc_icon.texture = icon
+
+
+# BOX ----------------------------------------------------------------------------------------------
 
 func _appear() -> void:
 	pass
