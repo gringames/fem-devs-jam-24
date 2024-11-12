590b3d45d681a1640de8ceb513e5f9809488e391
Kerstin Laptop <kerstin@irox-games.com>
2024-11-12 18:53:50 +0100

    add flicker sounds and flicker 2 times

--- femdevsjam24/Game/flicker.gd
+++ femdevsjam24/Game/flicker.gd
@@ -4,25 +4,35 @@ class_name Flicker
 @export var flicker_interval_min: float = 3
 @export var flicker_interval_max: float = 8
 @export var flicker_duration: float = 0.08
+@export var flicker_pause: float = 0.23
 @export var flicker_speed: float = 4
 
+var flicker_sfx: AudioStreamPlayer
 var fade: BlackFade
 
 
 func _ready() -> void:
+	flicker_sfx = $flickerSFX
 	fade = $BlackFadeCanvasLayer
 	fade.fade_speed = flicker_speed
 	await get_tree().create_timer(_get_random_in_interval()).timeout
-	_flicker()
+	_flicker_two_times()
 	
+	
+func _flicker_two_times() -> void:
+	flicker_sfx.play()
+	_flicker()
+	await get_tree().create_timer(flicker_pause).timeout
+	_flicker()
+	# wait a random amount of time and then trigger the flicker again
+	await get_tree().create_timer(_get_random_in_interval()).timeout
+	_flicker_two_times()
 
+# what a mess
 func _flicker() -> void:
 	fade.fade_to_black()
 	await get_tree().create_timer(flicker_duration).timeout
 	fade.fade_from_black()
-	fade.fade_from_black()
-	await get_tree().create_timer(_get_random_in_interval()).timeout
-	_flicker()
 
 
 func _get_random_in_interval() -> float:
