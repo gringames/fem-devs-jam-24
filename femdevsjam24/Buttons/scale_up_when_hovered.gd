fc2c12e146d9acb179265a0e64a699db70eb86e6
Kerstin Laptop <kerstin@irox-games.com>
2024-12-17 20:33:52 +0100

    use button icons from Lari

--- femdevsjam24/Buttons/scale_up_when_hovered.gd
+++ femdevsjam24/Buttons/scale_up_when_hovered.gd
@@ -2,12 +2,14 @@ extends Node2D
 class_name ScalableButton
 
 @onready var button: Button = get_parent()
+@export var change_pivot: bool = true
 
 
 func _ready() -> void:
 	button.connect("mouse_entered", _scale_up)
 	button.connect("mouse_exited", _scale_down)
-	button.pivot_offset = Vector2(button.size / 2)
+	if change_pivot:
+		button.pivot_offset = Vector2(button.size / 2)
 
 func _scale_up() -> void:
 	button.scale *= 1.2
